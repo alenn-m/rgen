b4cc1fe4a8d9d0fcf6dad04054fa0a2f6a0a2411
Alen Masic <alenn.masic@gmail.com>
2020-10-10 17:16:19 +0200

    WIP

--- util/draft/draft.go
+++ util/draft/draft.go
@@ -6,8 +6,9 @@ type Draft struct {
 
 type ModelOptions struct {
 	Properties     map[string]string   `yaml:"Properties"`
-	SkipController bool                `yaml:"SkipController"`
 	Validation     map[string][]string `yaml:"Validation"`
 	Actions        []string            `yaml:"Actions"`
 	Relationships  map[string]string   `yaml:"Relationships"`
+	SkipController bool                `yaml:"SkipController"`
+	Public         bool                `yaml:"Public"`
 }
